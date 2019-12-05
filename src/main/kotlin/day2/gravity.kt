9704ac975433abf516223270d479544d8695ef32
Lukas Taake <lukas.taake@holisticon.de>
2019-12-05 23:51:43 +0100

    Solve day 2 part 1

--- src/main/kotlin/day2/gravity.kt
+++ src/main/kotlin/day2/gravity.kt
@@ -1,2 +1,44 @@
 package day2
 
+import java.io.File
+
+fun main() {
+    val lines = File("src/main/resources/day2/input.txt").readLines()
+    val program: List<Int> = lines
+        .filter(String::isNotEmpty)
+        .flatMap { it.split(",") }
+        .map { it.toInt() }
+        .mapIndexed { i, v ->
+            when (i) {
+                1 -> 12
+                2 -> 2
+                else -> v
+            }
+        }
+    print(intcodes(program)[0])
+}
+
+fun intcodes(input: List<Int>): List<Int> {
+    return try {
+        (0..input.size step 4)
+            .fold(input, ::exec)
+    } catch (e: HaltingException) {
+        e.codes
+    }
+}
+
+fun exec(codes: List<Int>, i: Int): List<Int> =
+    when (codes[i]) {
+        1 -> codes.execAt(i) { a, b -> a + b }
+        2 -> codes.execAt(i) { a, b -> a * b }
+        99 -> throw HaltingException(codes)
+        else -> throw IllegalStateException("Unknown opcode ${codes[i]}")
+    }
+
+fun List<Int>.execAt(i: Int, op: ((Int, Int) -> Int)) =
+    replace(this[i+3], op(this[this[i+1]], this[this[i+2]]))
+
+fun <T> List<T>.replace(index: Int, updated: T) =
+    mapIndexed { i, current -> if (i == index) updated else current }
+
+data class HaltingException(val codes: List<Int>) : RuntimeException()
--- src/test/kotlin/day2/Day2Test.kt
+++ src/test/kotlin/day2/Day2Test.kt
@@ -1,6 +1,47 @@
 package day2
 
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+fun opcode(input: String): String {
+    val ints = input.split(",").map {it.toInt()}
+
+    return intcodes(ints).joinToString(",") { it.toString() }
+}
+
 class Day2Test {
 
+    @Test
+    fun add() {
+        // given:
+        val input = "1,0,0,0,99"
+        val expected = "2,0,0,0,99"
 
+        // then:
+        assertEquals(expected, opcode(input))
+    }
+
+    @Test
+    fun multiply() {
+        val input = "2,3,0,3,99"
+        val expected = "2,3,0,6,99"
+
+        assertEquals(expected, opcode(input))
+    }
+
+    @Test
+    fun exitNotLastItem() {
+        val input = "2,4,4,5,99,0"
+        val expected = "2,4,4,5,99,9801"
+
+        assertEquals(expected, opcode(input))
+    }
+
+    @Test
+    fun earlyExit() {
+        val input = "1,1,1,4,99,5,6,0,99"
+        val expected = "30,1,1,4,2,5,6,0,99"
+
+        assertEquals(expected, opcode(input))
+    }
 }
